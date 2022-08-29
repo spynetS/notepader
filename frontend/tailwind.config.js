cfd4cfa83a85fdda8c355c0904f82f952bf8baff
Alfred Roos <alfred@stensatter.se>
2022-08-29 12:18:41 +0200

    tailwind working

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
+
+/** @type {import('tailwindcss').Config} */  module.exports = {  content: [  "./src/**/*.{js,jsx,ts,tsx}",  ],  theme: {  extend: {},  },  plugins: [], } 
+
