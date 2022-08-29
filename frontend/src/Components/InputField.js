4043ac6d0347c5018c062af83ab58f769057f97c
Alfred Roos <alfred@stensatter.se>
2022-08-29 13:03:24 +0200

    update

--- frontend/src/Components/InputField.js
+++ frontend/src/Components/InputField.js
@@ -13,7 +13,7 @@ export default class InputField extends React.Component{
 
     render(){
         return(<div>
-            <input value={this.state.value} className={" rounded-lg bg-gray-700 p-2  outline-none text-white text-md ${this.props.className}"} placeholder={(this.props.placeholder)?this.props.placeholder:""} onChange={e=>{try{this.props.value(e.target.value);this.setState({value:e.target.value})}catch(error){this.setState({value:e.target.value})}}} ></input>
+            <input value={this.state.value} className={`rounded-lg bg-[#6D6D6D] p-2  outline-none text-white text-md ${this.props.className}`} placeholder={(this.props.placeholder)?this.props.placeholder:""} onChange={e=>{try{this.props.value(e.target.value);this.setState({value:e.target.value})}catch(error){this.setState({value:e.target.value})}}} ></input>
         </div>)
 
     }
--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -7,8 +7,11 @@ export default class Login extends React.Component
 
 
     render(){
-        return(<div className="  p-72" >
-            <InputField className="shadow-xl" ></InputField>
+        return(<div className="  p-72 flex flex-col gap-4" >
+            <InputField className="shadow-xl" placeholder="udername"  ></InputField>
+            <InputField className="shadow-xl" placeholder="udername" ></InputField>
+            <InputField className="shadow-xl" placeholder="udername" ></InputField>
+            <InputField className="shadow-xl" placeholder="udername" ></InputField>
         </div>)
     }
 
