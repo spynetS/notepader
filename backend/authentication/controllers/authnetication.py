99a5b3a377ea2b2c868d55c94f543f085c45e9d0
sharkoomaster <alirashini2323@outlook.com>
2022-09-20 12:36:02 +0200

    backend

--- backend/authentication/controllers/authnetication.py
+++ backend/authentication/controllers/authnetication.py
@@ -1,2 +1,15 @@
 from django.http import HttpResponse
+from authentication.models import user
+from authentication.models import u_session
 
+import json
+from passlib.hash import argon2
+
+def createUser(request):
+    req = json.loads(request)
+    _user    = user(username= req["username"], email= req["email"], password= argon2.hash(req["password"]))
+    _session = u_session(uId= _user.id, data= json.dumps({"username": req["username"], "email": req["email"]}))
+    return HttpResponse(_session.id)
+
+def loginUser(request):
+    req = json.loads(request)
--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -6,3 +6,7 @@ class user(models.Models):
     username = models.TextField(default="", null= False)
     email    = models.EmailField(default="", null= False)
     password = models.TextField(default="", null= False)
+
+class u_session(models.Models):
+    uId      = models.TextField(default="", null = False)
+    data     = models.TextField(default="{}", null= False)
