bae5b11c6085aada1aa2e6849edad3ba89fe19c0
Alfred Roos <alfred@stensatter.se>
2022-09-26 12:31:10 +0200

    added files and changed name of user to User

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Models):
+class User(models.Models):
     username = models.TextField(default="", null= False)
     email    = models.EmailField(default="", null= False)
     password = models.TextField(default="", null= False)
 
-class u_session(models.Models):
+class U_session(models.Models):
     uId      = models.TextField(default="", null = False)
     data     = models.TextField(default="{}", null= False)
---
+++ backend/files/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class File (models.Model):
+
+    name = models.TextField()
+    data = models.TextField()
+    parent = models.ForeignKey(File, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
